1bf4091c7fdaa6d21ca27bd8738cb61fe74c590f
Martin Beier <Martin.Beier@gmx.de>
2023-12-03 18:14:22 +0100

    Primefaces: Fotogallerie Umstellung Jakarta EE 10

--- src/main/java/com/github/mbeier1406/gallery/GalleryBean.java
+++ src/main/java/com/github/mbeier1406/gallery/GalleryBean.java
@@ -14,13 +14,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
+
 import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.file.UploadedFile;
 
 /**
  *
@@ -76,7 +79,7 @@ public class GalleryBean implements Serializable {
                 byte[] buffer = new byte[BUFFER_SIZE];
 
                 int bulk;
-                inputStream = event.getFile().getInputstream();
+                inputStream = event.getFile().getInputStream();
                 while (true) {
                     bulk = inputStream.read(buffer);
                     if (bulk < 0) {
@@ -106,5 +109,56 @@ public class GalleryBean implements Serializable {
         }
         
     }
-    
+
+    private UploadedFile file;
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    public void upload() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        if (file != null) {
+            Path path = Paths.get(((ServletContext) externalContext.getContext())
+                    .getRealPath(File.separator + "resources" + File.separator + "photos" + File.separator));
+            FileOutputStream fileOutputStream;
+            InputStream inputStream;
+            try {
+                String fn = file.getFileName();
+                fileOutputStream = new FileOutputStream(path.toString() + File.separator + fn);
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+
+                int bulk;
+                inputStream = file.getInputStream();
+                while (true) {
+                    bulk = inputStream.read(buffer);
+                    if (bulk < 0) {
+                        break;
+                    }
+                    fileOutputStream.write(buffer, 0, bulk);
+                    fileOutputStream.flush();
+                }
+
+                fileOutputStream.close();
+                inputStream.close();
+
+                Photo new_photo = new Photo(fn, false);
+                photos.add(new_photo);
+
+                facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Files were successfully uploaded !", null));
+            } catch (FileNotFoundException ex) {
+                facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "File " + file.getFileName() + " cannot be found!", null));
+                Logger.getLogger(GalleryBean.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "File " + file.getFileName() + " cannot be uploaded!", null));
+                Logger.getLogger(GalleryBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
 }
