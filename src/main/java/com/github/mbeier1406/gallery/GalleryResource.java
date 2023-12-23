b3fb17b2ca16ab25c3d681ee2eacd0c8749bee4a
Martin Beier <Martin.Beier@gmx.de>
2023-12-23 22:12:20 +0100

    Primefaces: Fotogallerie Umstellung JakartaEE 10

--- src/main/java/com/github/mbeier1406/gallery/GalleryResource.java
+++ src/main/java/com/github/mbeier1406/gallery/GalleryResource.java
@@ -1,12 +1,17 @@
 package com.github.mbeier1406.gallery;
 
-import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import static jakarta.ws.rs.core.Response.Status.OK;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 /**
  * Ermöglicht das Hochladen von Photos per REST.
@@ -24,13 +29,26 @@ public class GalleryResource {
 
 	/**
 	 * Liefert den Pfad, unter dem Die Fotos abgelegt werden.
-	 * @return den Datipfad
+	 * @return den Dateipfad
 	 */
 	@Path("photo")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String uploadPhoto() {
 		return photoPath;
+	}
+
+	/**
+	 * Lädt ein Foto per REST PUT hoch.
+	 * @param fileName Dateiname
+	 * @param fileContent das Foto
+	 * @return den HTTP-Status
+	 */
+	@Path("upload")
+	@PUT
+	public Response upload(@PathParam("fileName") String name, byte[] fileContent) {
+		// TODO Implementierung
+		return Response.status(OK).build();
 	}
 
 }
